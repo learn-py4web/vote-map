5c32667cd1260fa001811bc6946881e4d6dd3e15
Luca de Alfaro <luca@dealfaro.org>
2020-08-17 17:40:27 -0700

    Gone to bulma.css

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,51 @@
 
 // Map and its initialization.
 function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+          center: {lat: 37, lng: -120},
+          zoom: 6
+        });
+    var infoWindow = new google.maps.InfoWindow();
+    geolocate(infoWindow, map);
 }
+
+function geolocate(infoWindow, map) {
+// Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        console.log("Getting location");
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log("Gotten position:", position.coords)
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            console.log("Found position:", pos)
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            console.log("some error");
+            handleLocationError(true, infoWindow, map.getCenter());
+        },
+            {timeout: 2000});
+    } else {
+        // Browser doesn't support Geolocation
+        console.log("nope");
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
 
 
 // This will be the object that will contain the Vue attributes
